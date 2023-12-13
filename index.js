43a9b78b8a161a8db196bc1e03a3cf792a1bf0ff
unknown <edgarrios412@gmail.com>
2023-12-13 10:32:50 -0500

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./src/app")
 const {conn} = require("./src/db")
 
 conn.sync({force:true}).then(() => {
-    app.listen(3002, () => {
+    app.listen(process.env.PORT, () => {
         console.log("Servidor en linea en el puerto 3002")
     })
 }, (error) => console.log(error))
