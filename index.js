0b0b5a1ca3cf180fba49cb077236d9a5166321db
unknown <edgarrios412@gmail.com>
2024-12-14 20:37:28 -0500

    push

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ cron.schedule("*/10 * * * *", async () => {
     // console.log("Peticiones realizadas")
 })
 
-conn.sync({force:true}).then(() => {
+conn.sync({alter:true}).then(() => {
     console.log("Conectado a la base de datos")
     app.listen(3001, () => {
         console.log("Servidor en linea en el puerto 3001")
